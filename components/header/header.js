dea2cb41b0fb834d08d5dade10a8200aa55691fb
Kevin Mars <kevin.eth.420@gmail.com>
2023-12-26 18:14:43 +0800

    first commit

---
+++ components/header/header.js
@@ -0,0 +1,26 @@
+import './header.css'
+import Image from 'next/image'
+import logo from '@/assets/materials/logo.png'
+
+const Header = () => {
+    return (
+        <div className='header__container'>
+            <a className='header__logo__container' href='/'>
+                <Image className='logo__img' src={logo} width={50} alt="logo_image" />
+                <div className='logo__name'>
+                    Ferrolink
+                    <span>Industries Co.</span>
+                </div>
+            </a>
+            <nav className='navbar__container'>
+                <a className='navbar__link' href='/#top'>Home</a>
+                <a className='navbar__link' href='/product/hammer'>Product</a>
+                <a className='navbar__link' href='/#about'>About</a>
+                <a className='navbar__link' href='/#service'>Services</a>
+                <a className='navbar__link' href='/#contact'>Contact</a>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
